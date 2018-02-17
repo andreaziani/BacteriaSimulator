1b993b09813f3b23e45d25c205801a87d869e2af
Simone <simone.barbaro2@studio.unibo.it>
2018-02-17 22:22:15 +0100

    added main class

---
+++ src/application/BacteriaSimulator.java
@@ -0,0 +1,12 @@
+package application;
+/**
+ *test main class.
+ */
+public class BacteriaSimulator {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
