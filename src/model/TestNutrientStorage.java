99b04746f5be17bbd238fdb37a302b7e60549c01
Simone <simone.barbaro2@studio.unibo.it>
2018-02-20 12:26:47 +0100

    test empty

--- src/model/TestNutrientStorage.java
+++ src/model/TestNutrientStorage.java
@@ -1,6 +1,23 @@
 package model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.Test;
+
+import utils.NotEnounghEnergyException;
+
 /**
  *Unit test for the NutrientStorage class.
  */
 public class TestNutrientStorage {
+
+    @Test
+    public void testEmpty() {
+        NutrientStorage storage = new NutrientStorage(n -> (() -> 0));
+        assertEquals(storage.getEnergyStored().getAmount(), 0);
+        assertTrue(storage.getNutrients().isEmpty());
+        assertThrows(NotEnounghEnergyException.class, () -> storage.takeEnergy(() -> 1));
+    }
 }
