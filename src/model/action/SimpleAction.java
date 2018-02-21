8daf086bc6cb3b230ca0b6c205236e8e1b6b78a3
Simone <simone.barbaro2@studio.unibo.it>
2018-02-21 10:14:07 +0100

    implemented AbstractAction

--- src/model/action/SimpleAction.java
+++ src/model/action/SimpleAction.java
@@ -2,24 +2,18 @@ package model.action;
 /**
  * An implementation of action that contains only the action type. Can only be used by Eat, Nothing and Replicate action types.
  */
-public class SimpleAction implements Action {
+public class SimpleAction extends AbstractAction {
 
-    private final ActionType type;
     /**
      * Create a simple action given an ActionType.
      * @param type an ActionType.
      * @throws IllegalArgumentExeption if type is not Eat, Nothing or Replicate
      */
     public SimpleAction(final ActionType type) {
+        super(type);
         if (type != ActionType.NOTHING && type != ActionType.EAT && type != ActionType.REPLICATE) {
             throw new IllegalArgumentException("Wrong action type");
         }
-        this.type = type;
-    }
 
-    @Override
-    public ActionType getType() {
-        return this.type;
     }
-
 }
