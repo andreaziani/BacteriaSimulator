d340222e220a18b84b9745b2b6436e38eb1672e7
Simone <simone.barbaro2@studio.unibo.it>
2018-02-21 09:46:18 +0100

    implemented BehaviorDecorator

---
+++ src/model/bacteria/BehaviorDecorator.java
@@ -0,0 +1,24 @@
+package model.bacteria;
+
+import model.action.Action;
+import model.perception.Perception;
+/**
+ *Abstract implementation of Behavior that follows the decorator pattern. The method chooseAction is delegated to the Behavior taken by the constructor.
+ */
+public abstract class BehaviorDecorator implements Behavior {
+
+    private final Behavior delegate;
+    /**
+     * Construct a new BehaviorDecorator by taking a delegate. 
+     * @param delegate a Behavior.
+     */
+    public BehaviorDecorator(final Behavior delegate) {
+        this.delegate = delegate;
+    }
+
+    @Override
+    public Action chooseAction(final Perception perception) {
+        return delegate.chooseAction(perception);
+    }
+
+}
