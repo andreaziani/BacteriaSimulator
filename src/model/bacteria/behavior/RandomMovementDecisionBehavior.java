193df6b94283177ff55e60cb2eb9f55aaebddb02
Simone <simone.barbaro2@studio.unibo.it>
2018-02-23 13:02:51 +0100

    refactoring

--- src/model/bacteria/behavior/RandomMovementDecisionBehavior.java
+++ src/model/bacteria/behavior/RandomMovementDecisionBehavior.java
@@ -4,14 +4,19 @@ import java.util.Random;
 
 import model.Direction;
 import model.action.ActionType;
+
 /**
  * A behavior for choosing out to move that prefers going in a random direction.
  */
 public class RandomMovementDecisionBehavior extends DecisionBehaviorDecorator implements MovementBehavior {
-/**
- * Construct a new RandomMovementDecisionBehavior by taking a delegate. It implements the decorator pattern.
- * @param delegate a Behavior that will make decisions and choose an action using this object's decisions.
- */
+    /**
+     * Construct a new RandomMovementDecisionBehavior by taking a delegate. It
+     * implements the decorator pattern.
+     * 
+     * @param delegate
+     *            a Behavior that will make decisions and choose an action using
+     *            this object's decisions.
+     */
     public RandomMovementDecisionBehavior(final AbstractDecisionBehavior delegate) {
         super(delegate);
     }
