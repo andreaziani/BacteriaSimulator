604c735e7cee37b897f19639bf2593916369714d
Simone <simone.barbaro2@studio.unibo.it>
2018-02-23 12:21:44 +0100

    implemented random movement behavior

--- src/model/bacteria/behavior/RandomMovementDecisionBehavior.java
+++ src/model/bacteria/behavior/RandomMovementDecisionBehavior.java
@@ -4,18 +4,23 @@ import java.util.Random;
 
 import model.Direction;
 import model.action.ActionType;
-import model.action.DirectionalActionImpl;
-
+/**
+ * A behavior for choosing out to move that prefers going in a random direction.
+ */
 public class RandomMovementDecisionBehavior extends DecisionBehaviorDecorator implements MovementBehavior {
-
-    public RandomMovementDecisionBehavior(AbstractDecisionBehavior delegate) {
+/**
+ * Construct a new RandomMovementDecisionBehavior by taking a delegate. It implements the decorator pattern.
+ * @param delegate a Behavior that will make decisions and choose an action using this object's decisions.
+ */
+    public RandomMovementDecisionBehavior(final AbstractDecisionBehavior delegate) {
         super(delegate);
     }
-    
+
     @Override
     protected void updateDecisionSet() {
         Random rand = new Random();
-        //this.getDecisionSet().add(new Decisionnew DirectionalActionImpl(ActionType.MOVE, Direction.values()[rand.nextInt(Direction.values().length)]));
+        this.getDecisionSet().add(DecisionFactory.directionalDecision(ActionType.MOVE,
+                Direction.values()[rand.nextInt(Direction.values().length)], 1));
         super.updateDecisionSet();
     }
 
