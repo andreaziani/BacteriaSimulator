41f1d6f85057c08156918303685c324cab5fdbe4
Simone <simone.barbaro2@studio.unibo.it>
2018-03-03 18:08:23 +0100

    implemented NearFoodMovementDecisionBehavior

---
+++ src/model/bacteria/behavior/NearFoodMovementDecisionBehavior.java
@@ -0,0 +1,41 @@
+package model.bacteria.behavior;
+
+import java.util.EnumMap;
+import java.util.Map;
+
+import model.Direction;
+import model.action.ActionType;
+import model.action.DirectionalActionImpl;
+
+/**
+ * A behavior for choosing out to move that assigns to each move direction a
+ * preference based on the percepted distance from the nearest foods.
+ */
+public class NearFoodMovementDecisionBehavior extends DecisionBehaviorDecorator implements MovementBehavior {
+    /**
+     * Construct a new NearFoodMovementDecisionBehavior by taking a delegate. It
+     * implements the decorator pattern.
+     * 
+     * @param delegate
+     *            a Behavior that will make decisions and choose an action using
+     *            this object's decisions.
+     */
+    public NearFoodMovementDecisionBehavior(final AbstractDecisionBehavior delegate) {
+        super(delegate);
+    }
+
+    @Override
+    protected void updateDecisions() {
+        cleanActionDecisions(a -> a.getType() == ActionType.MOVE);
+        final Map<Direction, Double> values = new EnumMap<>(Direction.class);
+        Double tempVal = 0.0;
+        for (final Direction d : Direction.values()) {
+            final Double dirVal = this.getCurrentPerception().distFromFood(d).orElse(0.0);
+            values.put(d, dirVal);
+            tempVal += dirVal;
+        }
+        final double maxVal = tempVal;
+        values.forEach((d, v) -> this.getDecisions().put(new DirectionalActionImpl(ActionType.MOVE, d), v / maxVal));
+        super.updateDecisions();
+    }
+}
