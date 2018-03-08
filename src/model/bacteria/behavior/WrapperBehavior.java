9be1da0a77dd68e47f062adaee8b58212b8438dc
Simone <simone.barbaro2@studio.unibo.it>
2018-03-08 20:47:54 +0100

    added new behavior interface

---
+++ src/model/bacteria/behavior/WrapperBehavior.java
@@ -0,0 +1,9 @@
+package model.bacteria.behavior;
+
+/**
+ * Represent a wrapper of a Behavior that evaluates its decisions and may change
+ * it accordingly to its preferences.
+ */
+public interface WrapperBehavior extends Behavior {
+
+}
