89702e1938a739ed106e014605f8eacc74c657b5
Andrea Ziani <andrea.ziani@studio.unibo.it>
2018-03-10 15:24:33 +0100

    created TestInteraction that test creation and insertion of food by the user

--- src/view/ViewImpl.java
+++ src/view/ViewImpl.java
@@ -2,6 +2,8 @@ package view;
 
 import java.util.Set;
 
+import controller.ObserverCreationOfFood;
+import controller.ObserverInsertionFromView;
 import model.Analisys;
 import view.food.ViewFood;
 /**
@@ -11,12 +13,16 @@ import view.food.ViewFood;
 public class ViewImpl implements View {
     private final ViewInteraction interaction = new ViewInteractionImpl();
     private final ObserverExistingFoods observer;
-    /**
-     * Constructor that build a View and initialize an observer.
-     * @param observer to add.
-     */
-    public ViewImpl(final ObserverExistingFoods observer) {
+/**
+ * Constructor that build a View and initializing her observers.
+ * @param observer that watch the set of existing foods.
+ * @param observerCreation that watch creation of food.
+ * @param observerInsertion that watch insertion of foods.
+ */
+    public ViewImpl(final ObserverExistingFoods observer, final ObserverCreationOfFood observerCreation, final ObserverInsertionFromView observerInsertion) {
         this.observer = observer;
+        this.interaction.addCreationObserver(observerCreation);
+        this.interaction.addInsertionObserver(observerInsertion);
     }
     @Override
     public void update(final ViewState state) {
