6a1fdce52ae1928f9aa7c814a1b009414d905341
Simone <simone.barbaro2@studio.unibo.it>
2018-03-12 10:08:17 +0100

    completed BehaviorDecoratorOption

--- src/model/bacteria/behavior/BehaviorDecoratorOption.java
+++ src/model/bacteria/behavior/BehaviorDecoratorOption.java
@@ -1,5 +1,24 @@
 package model.bacteria.behavior;
 
+/**
+ * enumeration representing all types of decorators a behavior can have.
+ */
 public enum BehaviorDecoratorOption {
-    
+    /**
+     * Represents a CostFilterDecisionBehavior.
+     */
+    COST_FILTER,
+    /**
+     * Represents a PreferentialDecisionBehavior relative to the ActionType MOVE.
+     */
+    PREFERENTIAL_MOVE,
+    /**
+     * Represents a PreferentialDecisionBehavior relative to the ActionType EAT.
+     */
+    PREFERENTIAL_EAT,
+    /**
+     * Represents a PreferentialDecisionBehavior relative to the ActionType
+     * REPLICATE.
+     */
+    PREFERENTIAL_REPLICATE;
 }
