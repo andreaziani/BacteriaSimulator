7c4b5178d783cd077633fd6b7c1f6ae1fa252594
Simone <simone.barbaro2@studio.unibo.it>
2018-03-14 21:48:55 +0100

    implemented new decisionMakeroption enumerator

--- src/model/bacteria/behavior/decisionmaker/DecisionMakerOption.java
+++ src/model/bacteria/behavior/decisionmaker/DecisionMakerOption.java
@@ -1,9 +1,48 @@
 package model.bacteria.behavior.decisionmaker;
 
+import model.action.ActionType;
+
 /**
  * enumeration representing all types of DecisionMaker a behavior can have. Each
  * correlated with the action it will decide.
  */
 public enum DecisionMakerOption {
-    
+    /**
+     * Represents an AlwaysEatDecisionMaker.
+     */
+    ALWAYS_EAT(ActionType.EAT),
+    /**
+     * Represents a PreferentialEatingDecisionMaker.
+     */
+    PREFERENTIAL_EATING(ActionType.EAT),
+    /**
+     * Represents a RandomMovementDecisionMaker.
+     */
+    RANDOM_MOVEMENT(ActionType.MOVE),
+    /**
+     * Represents a PreferentialMovementDecisionMaker.
+     */
+    PREFERENTIAL_MOVEMENT(ActionType.MOVE),
+    /**
+     * Represents an AlwaysReproduceDecisionMaker.
+     */
+    ALWAYS_REPLICATE(ActionType.REPLICATE),
+    /**
+     * Represents a RandomReproductionDecisionMaker.
+     */
+    RANDOM_REPLICATE(ActionType.REPLICATE);
+
+    private final ActionType type;
+
+    DecisionMakerOption(final ActionType type) {
+        this.type = type;
+    }
+
+    /**
+     * @return the ActionType this DecisionMaker will make decisions about.
+     */
+    public ActionType getType() {
+        return type;
+    }
+
 }
