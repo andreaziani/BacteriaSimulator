e5675134d5cc9732e62b4a9ed9abafe79217115f
Simone <simone.barbaro2@studio.unibo.it>
2018-03-14 21:43:27 +0100

    changed decisionmaker options gestion

---
+++ src/model/bacteria/behavior/decisionmaker/DecisionMakerFactory.java
@@ -0,0 +1,5 @@
+package model.bacteria.behavior.decisionmaker;
+
+public class DecisionMakerFactory {
+
+}
---
+++ src/model/bacteria/behavior/decisionmaker/DecisionMakerOption.java
@@ -0,0 +1,9 @@
+package model.bacteria.behavior.decisionmaker;
+
+/**
+ * enumeration representing all types of DecisionMaker a behavior can have. Each
+ * correlated with the action it will decide.
+ */
+public enum DecisionMakerOption {
+    
+}
