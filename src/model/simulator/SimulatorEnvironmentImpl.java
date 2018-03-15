eb23cddcc4a6c23ea0cdd757bf1b9395309b3db7
fedemengo <fede.mengo@hotmail.it>
2018-03-15 15:56:52 +0100

    delegate Bacteria update to BacteriaManager

--- src/model/simulator/SimulatorEnvironmentImpl.java
+++ src/model/simulator/SimulatorEnvironmentImpl.java
@@ -1,35 +1,28 @@
-package model;
+package model.simulator;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.EnumMap;
-import java.util.Optional;
-import java.util.Set;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-import model.bacteria.Bacteria;
+import model.Analisys;
+import model.Position;
+import model.State;
+import model.StateImpl;
 import model.food.ExistingFoodManager;
 import model.food.ExistingFoodManagerImpl;
 import model.food.Food;
 import model.food.FoodEnvironment;
 import model.food.FoodEnvironmentImpl;
-import model.food.FoodFactory;
-import model.food.FoodFactoryImpl;
-import model.perception.Perception;
-import model.perception.PerceptionImpl;
-import utils.EnvGeometry;
 
 /**
  * implementation of SimulatorEnvironment.
  *
  */
 public class SimulatorEnvironmentImpl implements SimulatorEnvironment {
-    private final int foodPerRound = 15;
+    private static final int FOOD_PER_ROUND = 15;
+    // TODO this should probably not be static OR (?)
+    private static final double COST_OF_LIVING = 2.0;
     private final ExistingFoodManager manager = new ExistingFoodManagerImpl();
     private final FoodEnvironment foodEnv = new FoodEnvironmentImpl(manager);
-    private final FoodFactory factory = new FoodFactoryImpl();
-    private final Map<Position, Bacteria> bacteria = new HashMap<>();
+    private final BacteriaManager bactManager = new BacteriaManagerImpl(foodEnv, COST_OF_LIVING);
     private final State state = new StateImpl(this.foodEnv);
 
     @Override
@@ -47,68 +40,17 @@ public class SimulatorEnvironmentImpl implements SimulatorEnvironment {
         return this.state;
     }
 
-    private void updateDeadBacteria() {
-        final Set<Position> toBeRemoved = this.bacteria.entrySet().stream()
-                                                            .filter(e -> e.getValue().isDead())
-                                                            .peek(e -> this.foodEnv.addFood(e.getValue().getInternalFood(this.factory), e.getKey()))
-                                                            .map(e -> e.getKey())
-                                                            .collect(Collectors.toSet());
-        this.bacteria.keySet().removeAll(toBeRemoved);
-    }
-
-    private Map<Direction, Double> closestFoodDistances(final Position bacteriaPos, final Map<Position, Food> foodsState) {
-        final double radius = this.bacteria.get(bacteriaPos).getPerceptionRadius();
-        final Map<Direction, Double> distsToFood = new EnumMap<Direction, Double>(Direction.class);
-
-        for (int y = (int) -Math.ceil(radius); y <= (int) Math.ceil(radius); ++y) {
-            for (int x = (int) -Math.ceil(radius); x <= (int) Math.ceil(radius); ++x) {
-                final Position currentPos = new PositionImpl(bacteriaPos.getX() + x, bacteriaPos.getY() + y);
-                final double distanceToPos = EnvGeometry.distance(currentPos, bacteriaPos);
-                if (distanceToPos <= radius && foodsState.containsKey(currentPos)) {
-                    final Direction closestDir = EnvGeometry.directionFromAngle(EnvGeometry.angle(bacteriaPos, currentPos));
-                    if (!distsToFood.containsKey(closestDir) || distanceToPos < distsToFood.get(closestDir)) {
-                        distsToFood.put(closestDir, distanceToPos);
-                    }
-                }
-            }
-        }
-        return distsToFood;
-    }
-
-    private Perception createPerception(final Position bacteriaPos, final Map<Position, Food> foodsState) {
-        final Optional<Food> foodInPosition = foodsState.containsKey(bacteriaPos) ? Optional.of(foodsState.get(bacteriaPos)) : Optional.empty();
-        final Map<Direction, Double> distsToFood = closestFoodDistances(bacteriaPos, foodsState);
-        return new PerceptionImpl(foodInPosition, distsToFood);
-    }
-
-    private void performAction(final Position bacteriaPos, final Bacteria bacteria) {
-        switch (bacteria.getAction().getType()) {
-        case MOVE: 
-            break;
-        case EAT:
-            break;
-        case REPLICATE: 
-            break;
-        default: 
-            break;
-        }
-    }
-
-    private void updateLivingBacteria() {
-        final Map<Position, Food> foodsState = this.foodEnv.getFoodsState();
-        this.bacteria.entrySet().stream()
-                                .peek(e -> e.getValue().setPerception(this.createPerception(e.getKey(), foodsState)))
-                                .forEach(e -> this.performAction(e.getKey(), e.getValue()));
-    }
-
     private void updateFood() {
-        IntStream.range(0, foodPerRound).forEach(x -> this.foodEnv.addRandomFood());
+        IntStream.range(0, FOOD_PER_ROUND).forEach(x -> this.foodEnv.addRandomFood());
+    }
+    
+    private void updateBacteria() {
+        this.bactManager.updateBacteria();
     }
 
     @Override
     public void update() {
-        this.updateDeadBacteria();
-        this.updateLivingBacteria();
+        this.updateBacteria();
         this.updateFood();
     }
 
@@ -117,5 +59,4 @@ public class SimulatorEnvironmentImpl implements SimulatorEnvironment {
         // TODO Auto-generated method stub
         return null;
     }
-
 }
