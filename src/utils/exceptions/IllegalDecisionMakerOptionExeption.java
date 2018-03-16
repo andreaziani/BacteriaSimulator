d8a8b922e5f550365d99ba4735391be70d161575
Simone <simone.barbaro2@studio.unibo.it>
2018-03-16 21:34:01 +0100

    added singleton instances to factory and exeptions

---
+++ src/utils/exceptions/IllegalDecisionMakerOptionExeption.java
@@ -0,0 +1,14 @@
+package utils.exceptions;
+
+/**
+ * An exception representing a request of creation of a DecisionMaker from an
+ * unknown DecisionMakerOption.
+ */
+public class IllegalDecisionMakerOptionExeption extends RuntimeException {
+
+    /**
+     * Automatically generated.
+     */
+    private static final long serialVersionUID = -4043602471512076181L;
+
+}
