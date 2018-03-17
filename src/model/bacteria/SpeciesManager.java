99c70a96c52cb90570ae55a773988d96a3afd649
Simone <simone.barbaro2@studio.unibo.it>
2018-03-17 09:36:37 +0100

    completed SpeciesManager interface

--- src/model/bacteria/SpeciesManager.java
+++ src/model/bacteria/SpeciesManager.java
@@ -1,10 +1,25 @@
 package model.bacteria;
 
+import java.util.Set;
 
 /**
  * Represents the component of the environment that stores all the Species
  * created and used in the simulation.
  */
 public interface SpeciesManager {
-    void addSpecies();
+    /**
+     * Add a Species to the manager.
+     * 
+     * @param species
+     *            a Species to be added.
+     * @throws AlreadyExistingSpeciesExeption
+     *             if the name given to the Species is already associated to another
+     *             Species.
+     */
+    void addSpecies(Species species);
+
+    /**
+     * @return a Set containing all the Species added to the manager.
+     */
+    Set<Species> getSpecies();
 }
