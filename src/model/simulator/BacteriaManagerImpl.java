0c9143b6c90adcbd755fa8ff1fb25ae3c288bf08
fedemengo <fede.mengo@hotmail.it>
2018-03-18 23:56:42 +0100

    refactor using new collision interface

--- src/model/simulator/BacteriaManagerImpl.java
+++ src/model/simulator/BacteriaManagerImpl.java
@@ -23,7 +23,6 @@ import model.food.FoodFactoryImpl;
 import model.perception.Perception;
 import model.perception.PerceptionImpl;
 import utils.EnvUtil;
-import utils.Collision;
 
 import org.apache.commons.lang3.tuple.ImmutablePair;
 
@@ -40,36 +39,42 @@ public class BacteriaManagerImpl implements BacteriaManager {
 
     /**
      * Constructor.
-     * @param foodEnv used to update food environment according to bacteria actions
-     * @param costOfLiving amount of energy that a Bacteria spend just to stay alive
+     * 
+     * @param foodEnv
+     *            used to update food environment according to bacteria actions
+     * @param costOfLiving
+     *            amount of energy that a Bacteria spend just to stay alive
      */
     public BacteriaManagerImpl(final FoodEnvironment foodEnv, final double costOfLiving) {
         this.foodEnv = foodEnv;
         this.energyForLiving = new EnergyImpl(costOfLiving);
     }
 
-    private Map<Direction, Double> closestFoodDistances(final Position bacteriaPos, final Map<Position, Food> foodsState) {
+    private Map<Direction, Double> closestFoodDistances(final Position bacteriaPos,
+            final Map<Position, Food> foodsState) {
         final double radius = this.bacteria.get(bacteriaPos).getPerceptionRadius();
         final int start = (int) -Math.ceil(radius);
         final int end = (int) Math.ceil(radius);
         final Map<Direction, Double> distsToFood = new EnumMap<Direction, Double>(Direction.class);
 
-        EnvUtil.positionStream(start, end, bacteriaPos).map(pos -> ImmutablePair.of(pos, EnvUtil.distance(pos, bacteriaPos)))
-                                                           .filter(posDistPair -> posDistPair.getRight() <= radius)
-                                                           .filter(posDistPair -> foodsState.containsKey(posDistPair.getLeft()))
-                                                           .map(posDistPair -> {
-                                                               final double angle = EnvUtil.angle(bacteriaPos, posDistPair.getLeft());
-                                                               final Direction dir = EnvUtil.angleToDir(angle);
-                                                               return ImmutablePair.of(dir, posDistPair.getRight());
-                                                           })
-                                                           .filter(dirDistPair -> !distsToFood.containsKey(dirDistPair.getLeft()) 
-                                                                                  || dirDistPair.getRight() < distsToFood.get(dirDistPair.getLeft()))
-                                                           .forEach(dirDist -> distsToFood.put(dirDist.getLeft(), dirDist.getRight()));
+        EnvUtil.positionStream(start, end, bacteriaPos)
+                .map(pos -> ImmutablePair.of(pos, EnvUtil.distance(pos, bacteriaPos)))
+                .filter(posDistPair -> posDistPair.getRight() <= radius)
+                .filter(posDistPair -> foodsState.containsKey(posDistPair.getLeft())).map(posDistPair -> {
+                    final double angle = EnvUtil.angle(bacteriaPos, posDistPair.getLeft());
+                    final Direction dir = EnvUtil.angleToDir(angle);
+                    return ImmutablePair.of(dir, posDistPair.getRight());
+                })
+                .filter(dirDistPair -> !distsToFood.containsKey(dirDistPair.getLeft())
+                        || dirDistPair.getRight() < distsToFood.get(dirDistPair.getLeft()))
+                .forEach(dirDist -> distsToFood.put(dirDist.getLeft(), dirDist.getRight()));
         return distsToFood;
     }
 
     private Perception createPerception(final Position bacteriaPos, final Map<Position, Food> foodsState) {
-        final Optional<Food> foodInPosition = foodsState.containsKey(bacteriaPos) ? Optional.of(foodsState.get(bacteriaPos)) : Optional.empty();
+        final Optional<Food> foodInPosition = foodsState.containsKey(bacteriaPos)
+                ? Optional.of(foodsState.get(bacteriaPos))
+                : Optional.empty();
         final Map<Direction, Double> distsToFood = closestFoodDistances(bacteriaPos, foodsState);
         return new PerceptionImpl(foodInPosition, distsToFood);
     }
@@ -80,13 +85,17 @@ public class BacteriaManagerImpl implements BacteriaManager {
         final ActionType actionType = action.getType();
 
         switch (actionType) {
-        case MOVE: actionPerf.move();
+        case MOVE:
+            actionPerf.move();
             break;
-        case EAT: actionPerf.eat();
+        case EAT:
+            actionPerf.eat();
             break;
-        case REPLICATE: actionPerf.replicate();
+        case REPLICATE:
+            actionPerf.replicate();
             break;
-        default: actionPerf.doNothing();
+        default:
+            actionPerf.doNothing();
             break;
         }
         bacteria.spendEnergy(bacteria.getActionCost(action));
@@ -99,17 +108,14 @@ public class BacteriaManagerImpl implements BacteriaManager {
     private void updateLivingBacteria() {
         final Map<Position, Food> foodsState = this.foodEnv.getFoodsState();
         this.bacteria.entrySet().stream()
-                                .peek(e -> e.getValue().setPerception(this.createPerception(e.getKey(), foodsState)))
-                                .peek(e -> this.performAction(e.getKey(), e.getValue()))
-                                .forEach(e -> this.costOfLiving(e.getValue()));
+                .peek(e -> e.getValue().setPerception(this.createPerception(e.getKey(), foodsState)))
+                .peek(e -> this.performAction(e.getKey(), e.getValue())).forEach(e -> this.costOfLiving(e.getValue()));
     }
 
     private void updateDeadBacteria() {
-        final Set<Position> toBeRemoved = this.bacteria.entrySet().stream()
-                                                            .filter(e -> e.getValue().isDead())
-                                                            .peek(e -> this.foodEnv.addFood(e.getValue().getInternalFood(this.factory), e.getKey()))
-                                                            .map(e -> e.getKey())
-                                                            .collect(Collectors.toSet());
+        final Set<Position> toBeRemoved = this.bacteria.entrySet().stream().filter(e -> e.getValue().isDead())
+                .peek(e -> this.foodEnv.addFood(e.getValue().getInternalFood(this.factory), e.getKey()))
+                .map(e -> e.getKey()).collect(Collectors.toSet());
         this.bacteria.keySet().removeAll(toBeRemoved);
     }
 
@@ -128,13 +134,8 @@ public class BacteriaManagerImpl implements BacteriaManager {
     private class ActionPerformer {
         private Position bacteriaPos;
         private Bacteria bacteria;
-        private final Collision<Bacteria> bacteriaCollision;
 
         private ActionPerformer() {
-            this.bacteriaCollision = (p1, p2, map) -> {
-                final double distance = EnvUtil.distance(p1, p2);
-                return (distance <= map.get(p1).getRadius() || distance <= map.get(p2).getRadius());
-            };
         }
 
         private void setStatus(final Position bacteriaPos, final Bacteria bacteria) {
@@ -147,9 +148,8 @@ public class BacteriaManagerImpl implements BacteriaManager {
         }
 
         private void eat() {
-            final Optional<Food> foodInPosition = BacteriaManagerImpl.this.foodEnv.getFoodsState().containsKey(this.bacteriaPos)
-                                                        ? Optional.of(foodEnv.getFoodsState().get(this.bacteriaPos)) 
-                                                            : Optional.empty();
+            final Optional<Food> foodInPosition = BacteriaManagerImpl.this.foodEnv.getFoodsState().containsKey(
+                    this.bacteriaPos) ? Optional.of(foodEnv.getFoodsState().get(this.bacteriaPos)) : Optional.empty();
             if (foodInPosition.isPresent()) {
                 this.bacteria.addFood(foodInPosition.get());
                 BacteriaManagerImpl.this.foodEnv.removeFood(foodInPosition.get(), this.bacteriaPos);
@@ -160,10 +160,13 @@ public class BacteriaManagerImpl implements BacteriaManager {
             final double bacteriaRadius = this.bacteria.getRadius();
             final int start = (int) -Math.ceil(bacteriaRadius * 2);
             final int end = (int) Math.ceil(bacteriaRadius * 2);
+
             final Optional<Position> freePosition = EnvUtil.positionStream(start, end, this.bacteriaPos)
-                                                     .filter(position -> !BacteriaManagerImpl.this.bacteria.containsKey(position))
-                                                     .filter(position -> !EnvUtil.<Bacteria>isCollision(position, this.bacteriaPos, BacteriaManagerImpl.this.bacteria, this.bacteriaCollision))
-                                                     .findAny();
+                    .filter(position -> !BacteriaManagerImpl.this.bacteria.containsKey(position))
+                    .filter(position -> !EnvUtil.isCollision(
+                            ImmutablePair.of(position, BacteriaManagerImpl.this.bacteria.get(position)),
+                            ImmutablePair.of(this.bacteriaPos, this.bacteria)))
+                    .findAny();
 
             if (freePosition.isPresent()) {
                 final GeneticCode clonedGenCode = this.bacteria.getGeneticCode().clone();
