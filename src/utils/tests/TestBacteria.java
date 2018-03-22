0a2a45ebc8929b21c17af80530a2d56e7434949d
Simone <simone.barbaro2@studio.unibo.it>
2018-03-22 18:17:09 +0100

    tested build and reset

--- src/utils/tests/TestBacteria.java
+++ src/utils/tests/TestBacteria.java
@@ -1,8 +1,43 @@
 package utils.tests;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.Test;
+
+import model.bacteria.Species;
+import model.bacteria.SpeciesBuilder;
+
 /**
  * Unit test for the Bacteria and Species of the model.
  */
 public class TestBacteria {
+    private static final String NAME_1 = "name";
+    private static final String NAME_2 = "new name";
 
+    /**
+     * Test corrected functioning of build and reset of SpeciesBuilder.
+     */
+    @Test
+    public void testBuildSpecies() {
+        final SpeciesBuilder builder = new SpeciesBuilder(NAME_1);
+        Species species = tryBuild(builder);
+        assertEquals(NAME_1, species.getName());
+
+        assertThrows(IllegalStateException.class, builder::build);
+        builder.reset(NAME_2);
+        species = tryBuild(builder);
+        assertEquals(NAME_2, species.getName());
+    }
+
+    private Species tryBuild(final SpeciesBuilder builder) {
+        Species species = null;
+        try {
+            species = builder.build();
+        } catch (Exception e) {
+            fail("should have worked");
+        }
+        return species;
+    }
 }
