cdb85cde0aa01a24472f9454b7942dfe47dfaa02
Andrea Ziani <andrea.ziani@studio.unibo.it>
2018-03-29 08:55:42 +0200

    changed package view.view to view.gui

--- src/view/gui/MainFrame.java
+++ src/view/gui/MainFrame.java
@@ -1,4 +1,4 @@
-package view.view;
+package view.gui;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
--- src/view/gui/SimulationPanel.java
+++ src/view/gui/SimulationPanel.java
@@ -1,4 +1,4 @@
-package view.view;
+package view.gui;
 
 //import java.awt.Color;
 import java.awt.Graphics;
