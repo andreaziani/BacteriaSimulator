aa2b2b33c69074f13671e1970866a4cf7c1fcc1d
Simone <simone.barbaro2@studio.unibo.it>
2018-04-01 17:32:56 +0200

    documented InitialState

--- src/controller/InitialState.java
+++ src/controller/InitialState.java
@@ -11,6 +11,9 @@ import view.model.bacteria.ViewBacteriaImpl;
 import view.model.bacteria.ViewSpecies;
 import view.model.food.ViewFoodImpl;
 
+/**
+ * Represents all the information needed for a simulation to start.
+ */
 public class InitialState {
     private Map<PositionImpl, ViewBacteriaImpl> bacteriaMap;
     private Map<PositionImpl, ViewFoodImpl> foodMap;
@@ -19,7 +22,15 @@ public class InitialState {
     private final double maxX;
     private final double maxY;
 
-    public InitialState(double maxX, double maxY) {
+    /**
+     * Create an InitialState from the size of the simulation.
+     * 
+     * @param maxX
+     *            the maximum size of the x coordinate.
+     * @param maxY
+     *            the maximum size of the y coordinate.
+     */
+    public InitialState(final double maxX, final double maxY) {
         bacteriaMap = new HashMap<>();
         foodMap = new HashMap<>();
         existingFood = new HashSet<>();
@@ -28,36 +39,70 @@ public class InitialState {
         this.maxY = maxY;
     }
 
-    public void setState(HashMap<PositionImpl, ViewBacteriaImpl> bacteriaMap,
-            HashMap<PositionImpl, ViewFoodImpl> foodMap) {
+    /**
+     * Set the state of the simulation in the form of maps of positions and View
+     * representation of the simulation objects.
+     * 
+     * @param bacteriaMap
+     *            the map of all bacteria.
+     * @param foodMap
+     *            the map of all foods.
+     */
+    public void setState(final Map<PositionImpl, ViewBacteriaImpl> bacteriaMap, final Map<PositionImpl, ViewFoodImpl> foodMap) {
         this.bacteriaMap = bacteriaMap;
         this.foodMap = foodMap;
     }
 
-    public void addFood(ViewFoodImpl food) {
+    /**
+     * @param food
+     *            a food to be added to the set that contains all the foods created
+     *            by the user.
+     */
+    public void addFood(final ViewFoodImpl food) {
         existingFood.add(food);
     }
 
-    public void addSpecies(ViewSpecies species) {
+    /**
+     * @param species
+     *            a species to be added to the set that contains all the species
+     *            created by the user.
+     */
+    public void addSpecies(final ViewSpecies species) {
         this.species.add(species);
     }
 
+    /**
+     * @return the positions of all bacteria and their view representation.
+     */
     public Map<PositionImpl, ViewBacteriaImpl> getBacteriaMap() {
         return bacteriaMap;
     }
 
+    /**
+     * @return the positions of all foods and their view representation.
+     */
     public Map<PositionImpl, ViewFoodImpl> getFoodMap() {
         return foodMap;
     }
 
+    /**
+     * @return all the foods created by the user.
+     */
     public Set<ViewFoodImpl> getExistingFood() {
         return existingFood;
     }
 
+    /**
+     * @return all the species created by the users.
+     */
     public Set<ViewSpecies> getSpecies() {
         return species;
     }
 
+    /**
+     * @return a Position representing the opposite edge of the (0, 0) in the
+     *         environment.
+     */
     public Position getMaxPosition() {
         return new PositionImpl(maxX, maxY);
     }
