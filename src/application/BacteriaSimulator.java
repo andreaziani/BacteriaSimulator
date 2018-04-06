3164aadfc3d2dcb55743bdb6ac435d73de6bc776
fedemengo <fede.mengo@hotmail.it>
2018-04-06 21:56:45 +0200

    first implementaion of application entry point

--- src/application/BacteriaSimulator.java
+++ src/application/BacteriaSimulator.java
@@ -1,12 +1,33 @@
 package application;
+
+import controller.Controller;
+import controller.ControllerImpl;
+import view.ViewImpl;
+import view.gui.MainFrame;
+
 /**
  *test main class.
  */
-public class BacteriaSimulator {
+public final class BacteriaSimulator {
 
-    public static void main(String[] args) {
-        // TODO Auto-generated method stub
+    private final Controller controller;
+    private final MainFrame mainFrame;
 
+    private BacteriaSimulator() {
+        this.controller = ControllerImpl.getController();
+        this.mainFrame = new MainFrame(new ViewImpl(this.controller));
     }
 
+    private void start() {
+        //this.controller.init();
+    }
+
+    /**
+     * Entry point of the application.
+     * @param args additional argument
+     */
+    public static void main(final String[] args) {
+        final BacteriaSimulator application = new BacteriaSimulator();
+        application.start();
+    }
 }
