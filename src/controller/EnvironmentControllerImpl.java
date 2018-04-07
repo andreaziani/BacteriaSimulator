c95ef263b9d9b4949c91aff7845b3d2f0d4e9588
fedemengo <fede.mengo@hotmail.it>
2018-04-07 11:57:44 +0200

    minor changes in simulation loop

--- src/controller/EnvironmentControllerImpl.java
+++ src/controller/EnvironmentControllerImpl.java
@@ -43,15 +43,20 @@ public class EnvironmentControllerImpl implements EnvironmentController {
             @Override
             public void run() {
                 // TODO different condition
-                while (true) {
+                while (!env.getState().getBacteriaState().isEmpty()) {
+                    final long start = System.currentTimeMillis();
                     env.update();
-
-                    final State state = env.getState();
-                    Log.getLog().info("Bacteria " + state.getBacteriaState().toString());
-                    Log.getLog().info("Food " + state.getFoodsState().toString());
+                    final long elapsed = System.currentTimeMillis() - start;
+                    
+                    //final State state = env.getState();
+                    //Log.getLog().info("Bacteria " + state.getBacteriaState().toString());
+                    //Log.getLog().info("Food " + state.getFoodsState().toString());
 
                     try {
-                        Thread.sleep(PERIOD);
+                        if(elapsed < PERIOD) {
+                            System.out.println(elapsed);
+                            Thread.sleep(PERIOD - elapsed);
+                        }
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
