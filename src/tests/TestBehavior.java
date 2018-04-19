5f973c892a4df64a937e453dbe4f4702ae43c21b
Simone <simone.barbaro2@studio.unibo.it>
2018-04-19 18:56:13 +0200

    refactoring

--- src/tests/TestBehavior.java
+++ src/tests/TestBehavior.java
@@ -42,14 +42,17 @@ public class TestBehavior {
             nutrientToEnergyConverter = TestUtils.allNutrientsBad();
             return this;
         }
+
         KnowledgeBuilder setActionCostFunction(final Function<Action, Energy> function) {
             actionCostFunction = function;
             return this;
         }
+
         KnowledgeBuilder setPerceptionDirections(final Map<Direction, Double> perceptionDirections) {
             this.perceptionDirections = perceptionDirections;
             return this;
         }
+
         BacteriaKnowledge build() {
             final BacteriaKnowledge result = new BacteriaKnowledge(
                     new PerceptionImpl(Optional.of(TestUtils.getAFood()), perceptionDirections),
@@ -66,11 +69,12 @@ public class TestBehavior {
         final BacteriaKnowledge knowledge = new KnowledgeBuilder()
                 .setPerceptionDirections(TestUtils.bestDirection(Direction.NORTH)).setNutrientsBad().build();
         AbstractDecisionBehavior behavior = TestUtils.baseBehaviorFromOptions(Collections.emptyList());
-        assertEquals("Behavior should choose to do nothing", ActionType.NOTHING, behavior.chooseAction(knowledge).getType());
+        assertEquals("Behavior should choose to do nothing", ActionType.NOTHING,
+                behavior.chooseAction(knowledge).getType());
         behavior = TestUtils.baseBehaviorFromOptions(
                 Arrays.asList(DecisionMakerOption.PREFERENTIAL_EATING, DecisionMakerOption.NEAR_FOOD_MOVEMENT));
         assertEquals(
-                "Behavior should choose to move north with the distance equals to that of the distance to the food", 
+                "Behavior should choose to move north with the distance equals to that of the distance to the food",
                 new DirectionalActionImpl(ActionType.MOVE, Direction.NORTH,
                         knowledge.getCurrentPerception().distFromFood(Direction.NORTH).get()),
                 behavior.chooseAction(knowledge));
@@ -90,11 +94,11 @@ public class TestBehavior {
         AbstractDecisionBehavior behavior = TestUtils
                 .baseBehaviorFromOptions(Arrays.asList(DecisionMakerOption.ALWAYS_EAT,
                         DecisionMakerOption.NEAR_FOOD_MOVEMENT, DecisionMakerOption.ALWAYS_REPLICATE));
-        assertNotEquals("Behavior should not choose MOVE because other options have more value", 
-                ActionType.MOVE, behavior.chooseAction(knowledge).getType());
+        assertNotEquals("Behavior should not choose MOVE because other options have more value", ActionType.MOVE,
+                behavior.chooseAction(knowledge).getType());
         behavior = new CostFilterDecisionBehavior(behavior);
-        assertEquals("Behavior should choose MOVE because other options must be filtered out",
-                ActionType.MOVE, behavior.chooseAction(knowledge).getType());
+        assertEquals("Behavior should choose MOVE because other options must be filtered out", ActionType.MOVE,
+                behavior.chooseAction(knowledge).getType());
     }
 
     /**
@@ -109,11 +113,11 @@ public class TestBehavior {
                 .baseBehaviorFromOptions(Arrays.asList(DecisionMakerOption.PREFERENTIAL_EATING,
                         DecisionMakerOption.NEAR_FOOD_MOVEMENT, DecisionMakerOption.ALWAYS_REPLICATE));
         behavior = new CostFilterDecisionBehavior(behavior);
-        assertEquals("Behavior should choose EAT because has more vaule",
-                ActionType.EAT, behavior.chooseAction(knowledge).getType());
+        assertEquals("Behavior should choose EAT because has more vaule", ActionType.EAT,
+                behavior.chooseAction(knowledge).getType());
         behavior = new PreferentialDecisionBehavior(behavior, ActionType.MOVE);
-        assertEquals("Behavior should choose MOVE because has more vaule with the preference",
-                ActionType.MOVE, behavior.chooseAction(knowledge).getType());
+        assertEquals("Behavior should choose MOVE because has more vaule with the preference", ActionType.MOVE,
+                behavior.chooseAction(knowledge).getType());
     }
 
     /**
@@ -125,20 +129,19 @@ public class TestBehavior {
                 .setActionCostFunction(TestUtils.singleLargeCostActionType(ActionType.REPLICATE)).build();
         AbstractDecisionBehavior behavior = TestUtils
                 .baseBehaviorFromOptions(Collections.singletonList(DecisionMakerOption.NEAR_FOOD_MOVEMENT));
-        assertEquals("Behavior should choose NOTHING because it can't see foods",
-                ActionType.NOTHING, behavior.chooseAction(knowledge).getType());
+        assertEquals("Behavior should choose NOTHING because it can't see foods", ActionType.NOTHING,
+                behavior.chooseAction(knowledge).getType());
         behavior = new ExplorerDecisionBehavior(behavior);
-        assertEquals("Behavior should choose MOVE because the explorer will decide so",
-                ActionType.MOVE, behavior.chooseAction(knowledge).getType());
+        assertEquals("Behavior should choose MOVE because the explorer will decide so", ActionType.MOVE,
+                behavior.chooseAction(knowledge).getType());
 
-        behavior = 
-                TestUtils.baseBehaviorFromOptions(
-                        Arrays.asList(DecisionMakerOption.PREFERENTIAL_EATING, DecisionMakerOption.NEAR_FOOD_MOVEMENT));
+        behavior = TestUtils.baseBehaviorFromOptions(
+                Arrays.asList(DecisionMakerOption.PREFERENTIAL_EATING, DecisionMakerOption.NEAR_FOOD_MOVEMENT));
         assertEquals("Behavior should choose EAT because it can't see foods in other direction and can eat",
                 ActionType.EAT, behavior.chooseAction(knowledge).getType());
         behavior = new PreferentialDecisionBehavior(new ExplorerDecisionBehavior(behavior), ActionType.MOVE);
-        assertEquals("Behavior should choose EAT because it prefers moving and will want to explore",
-                ActionType.MOVE, behavior.chooseAction(knowledge).getType());
+        assertEquals("Behavior should choose EAT because it prefers moving and will want to explore", ActionType.MOVE,
+                behavior.chooseAction(knowledge).getType());
 
     }
 }
