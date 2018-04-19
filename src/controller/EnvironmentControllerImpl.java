a9b745d96d2bc9708d16e050d53086cde7db8d64
fedemengo <fede.mengo@hotmail.it>
2018-04-19 18:12:20 +0200

    First implementation of SimulationLoop as separate class

--- src/controller/EnvironmentControllerImpl.java
+++ src/controller/EnvironmentControllerImpl.java
@@ -28,64 +28,31 @@ import view.model.food.ViewFood;
  *
  */
 public abstract class EnvironmentControllerImpl implements EnvironmentController {
-    private static final long PERIOD = 125L;
     private Environment environment;
     private FoodController foodController;
     private Optional<ViewPosition> maxViewPosition = Optional.empty();
     private Replay replay;
     private SimulationState currentState;
-    private final SimulationLoop loop;
+    private SimulationLoop simLoop;
 
     /**
      * Constructor of EnvironmentController.
      */
     public EnvironmentControllerImpl() {
-
-        initialize();
-        this.loop = new SimulationLoop() {
-            private boolean condition;
-
-            @Override
-            public void run() {
-                condition = true;
-                while (condition) {
-                    final long start = System.currentTimeMillis();
-                    State simulationState;
-                    synchronized (EnvironmentControllerImpl.this) {
-                        environment.update();
-                        simulationState = environment.getState();
-                        replay.addState(simulationState);
-                        simulationLoop();
-                        condition = !environment.isSimulationOver();
-                    }
-                    final long elapsed = System.currentTimeMillis() - start;
-                    // DUBUGGING INFO
-                    Logger.getInstance().info("GameLoop", "Elapsed: " + elapsed + " ms");
-                    Logger.getInstance().info("GameLoop", "Bact size: " + simulationState.getBacteriaState().size());
-                    Logger.getInstance().info("GameLoop", "Food size: " + simulationState.getFoodsState().size());
-                    if (elapsed < PERIOD) {
-                        try {
-                            Thread.sleep(PERIOD - elapsed);
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                }
-                updateCurrentState(SimulationState.ENDED);
-                replay.setAnalysis(environment.getAnalysis());
-            }
-
-            @Override
-            public void stop() {
-                condition = false;
-            }
-        };
+        initialize(Optional.empty());
+        //this.simLoop = new SimulationLoop(this, this.environment);
     }
 
-    private void initialize() {
-        this.currentState = SimulationState.NOT_READY;
-        this.environment = new SimulatorEnvironment();
+    private void initialize(final Optional<InitialState> initialState) {
+        //this.currentState = SimulationState.NOT_READY;
+        if(initialState.isPresent()) {
+            this.environment = new SimulatorEnvironment(initialState.get());
+        } else {            
+            this.environment = new SimulatorEnvironment();
+        }
         this.foodController = new FoodControllerImpl(this.environment);
+        this.simLoop = new SimulationLoop(this, this.environment);
+        this.currentState = SimulationState.NOT_READY;
     }
 
     @Override
@@ -93,11 +60,23 @@ public abstract class EnvironmentControllerImpl implements EnvironmentController
         this.startLoop();
     }
 
+    public synchronized void stop() {
+        this.simLoop.stop();
+    }
+
+    public synchronized void pause() {
+        this.simLoop.pause();
+    }
+
+    public synchronized void resume() {
+        this.simLoop.resume();
+    }
+
     private void startLoop() {
-        this.updateCurrentState(SimulationState.RUNNING);
+        //this.updateCurrentState(SimulationState.RUNNING);
         this.environment.initialize();
         replay = new Replay(this.environment.getInitialState());
-        final Thread mainThread = new Thread(this.loop);
+        final Thread mainThread = new Thread(this.simLoop);
 
         Logger.getInstance().info("Controller", "Application started");
         mainThread.start();
@@ -106,7 +85,7 @@ public abstract class EnvironmentControllerImpl implements EnvironmentController
     @Override
     public final void resetSimulation() {
         updateCurrentState(SimulationState.NOT_READY);
-        initialize();
+        //initialize(Optional.empty());
     }
 
     /**
@@ -118,9 +97,7 @@ public abstract class EnvironmentControllerImpl implements EnvironmentController
      *            the representation of the initial state.
      */
     protected void setInitialState(final InitialState initialState) {
-        this.resetSimulation();
-        environment = new SimulatorEnvironment(initialState);
-        foodController = new FoodControllerImpl(environment);
+        this.initialize(Optional.of(initialState));
         if (initialState.getExistingFood().isEmpty() || initialState.getSpecies().isEmpty()) {
             this.updateCurrentState(SimulationState.NOT_READY);
         } else if (initialState.hasState()) { //Better to set in PAUSED
@@ -154,14 +131,14 @@ public abstract class EnvironmentControllerImpl implements EnvironmentController
     /**
      * method intended for allowing EnvController and Controller to "communicate".
      */
-    protected abstract void simulationLoop();
+    public abstract void simulationLoop();
 
     /**
      * @param replay
      *            a replay from which to construct a ReplayEnvironment.
      */
     protected void startReplay(final Replay replay) {
-        environment = new ReplayEnvironment(replay);
+        this.environment = new ReplayEnvironment(replay);
         start();
     }
 
@@ -220,15 +197,32 @@ public abstract class EnvironmentControllerImpl implements EnvironmentController
         this.getEnvironmentAsInteractive().setFoodDistributionStrategy(strategy);
     }
 
+    /*####################################################*/
+
     /**
      * Update the current state of the simulation.
      * 
      * @param state
      *            the state that the simulation'll assume.
      */
-    protected void updateCurrentState(final SimulationState state) {
+    public void updateCurrentState(final SimulationState state) {
         this.currentState = state;
+        if (this.currentState == SimulationState.ENDED) {
+            this.replay.setAnalysis(environment.getAnalysis());
+        }
     }
+
+
+    public SimulationState getCurrentState() {
+        return this.currentState;
+    }
+
+    @Override
+    public void addReplayState(final State currentState) {
+        this.replay.addState(currentState);
+    }
+
+    /*####################################################*/
 
     @Override
     public Set<SpeciesOptions> getSpecies() {
