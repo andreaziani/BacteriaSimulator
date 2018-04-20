9ad89f65255872f3d1da100a271d8e255928111b
fedemengo <fede.mengo@hotmail.it>
2018-04-20 03:52:51 +0200

    Refactor and minor improvements

--- src/model/simulator/ActionManager.java
+++ src/model/simulator/ActionManager.java
@@ -69,18 +69,15 @@ public class ActionManager extends RecursiveAction {
         this.actionPerformer = actionPerf;
     }
 
-    private Map<Direction, Double> closestFoodDistances(final Position bacteriaPos,
-            final Map<Position, Food> foodsState) {
+    private Map<Direction, Double> closestFoodDistances(final Position bacteriaPos) {
         final int radius = (int) Math.ceil(this.bacteriaEnv.getBacteria(bacteriaPos).getPerceptionRadius());
         final Map<Direction, Double> distsToFood = new EnumMap<Direction, Double>(Direction.class);
 
-        EnvironmentUtil.positionStream(radius, bacteriaPos, maxPosition)
-                .map(pos -> Pair.of(pos, EnvironmentUtil.distance(pos, bacteriaPos)))
-                .filter(pairPosDist -> pairPosDist.getRight() <= radius)
-                .filter(pairPosDist -> foodsState.containsKey(pairPosDist.getLeft())).map(pairPosDist -> {
-                    final double angle = EnvironmentUtil.angle(bacteriaPos, pairPosDist.getLeft());
+        EnvironmentUtil.positionStream(radius, bacteriaPos, this.maxPosition)
+                .filter(pos -> this.foodsState.containsKey(pos)).map(pos -> {
+                    final double angle = EnvironmentUtil.angle(bacteriaPos, pos);
                     final Direction dir = EnvironmentUtil.angleToDir(angle);
-                    return Pair.of(dir, pairPosDist.getRight());
+                    return Pair.of(dir, EnvironmentUtil.distance(pos, bacteriaPos));
                 })
                 .filter(pairDirDist -> !distsToFood.containsKey(pairDirDist.getLeft())
                         || pairDirDist.getRight() < distsToFood.get(pairDirDist.getLeft()))
@@ -88,25 +85,26 @@ public class ActionManager extends RecursiveAction {
         return distsToFood;
     }
 
-    private Optional<Position> collidingFood(final Position bacteriaPos, final Map<Position, Food> foodsState) {
+    private Optional<Position> collidingFood(final Position bacteriaPos) {
         if (this.maxFoodRadius.isPresent()) {
             final int maxRadius = (int) Math.ceil(this.maxFoodRadius.get());
             final Bacteria bacteria = this.bacteriaEnv.getBacteria(bacteriaPos);
-            return EnvironmentUtil.positionStream(maxRadius, bacteriaPos, maxPosition)
-                    .filter(pos -> foodsState.containsKey(pos)).map(pos -> Pair.of(pos, foodsState.get(pos)))
+            return EnvironmentUtil.positionStream(maxRadius, bacteriaPos, this.maxPosition)
+                    .filter(pos -> this.foodsState.containsKey(pos)).map(pos -> Pair.of(pos, this.foodsState.get(pos)))
                     .filter(pairPosFood -> EnvironmentUtil.isCollision(Pair.of(bacteriaPos, bacteria), pairPosFood))
-                    .map(a -> a.getLeft()).findAny();
+                    .findAny().map(a -> a.getLeft());
         } else {
             return Optional.empty();
         }
     }
 
-    private Perception createPerception(final Position bacteriaPos, final Map<Position, Food> foodsState) {
-        final Optional<Position> foodPosition = collidingFood(bacteriaPos, foodsState);
+    private Perception createPerception(final Position bacteriaPos) {
+        final Optional<Position> foodPosition = collidingFood(bacteriaPos);
         this.foodsPosition.put(bacteriaPos, foodPosition);
-        final Optional<Food> foodInPosition = foodPosition.isPresent() ? Optional.of(foodsState.get(foodPosition.get()))
+        final Optional<Food> foodInPosition = foodPosition.isPresent()
+                ? Optional.of(this.foodsState.get(foodPosition.get()))
                 : Optional.empty();
-        final Map<Direction, Double> distsToFood = closestFoodDistances(bacteriaPos, foodsState);
+        final Map<Direction, Double> distsToFood = closestFoodDistances(bacteriaPos);
         return new PerceptionImpl(foodInPosition, distsToFood);
     }
 
@@ -119,22 +117,26 @@ public class ActionManager extends RecursiveAction {
     }
 
     private void performAction(final Position bacteriaPos, final Bacteria bacteria) {
+        // TODO remove
         synchronized (this.actionPerformer) {
             actionPerformer.setStatus(bacteriaPos, bacteria);
             final Action action = bacteria.getAction();
             final ActionType actionType = action.getType();
-            
+
             try {
                 bacteria.spendEnergy(bacteria.getActionCost(action));
                 switch (actionType) {
                 case MOVE:
+                    // TODO mutex for action that modify bacteriaEnv
                     final DirectionalAction moveAction = (DirectionalActionImpl) action;
                     actionPerformer.move(moveAction.getDirection(), moveAction.getDistance());
                     break;
                 case EAT:
+                    // TODO mutex for action that modify foodEnv
                     actionPerformer.eat(this.foodsPosition.get(bacteriaPos));
                     break;
                 case REPLICATE:
+                    // TODO mutex for action that modify bacteriaEnv
                     final int numberOfBacteria = this.bacteriaEnv.getNumberOfBacteria();
                     actionPerformer.replicate(numberOfBacteria);
                     break;
@@ -150,33 +152,31 @@ public class ActionManager extends RecursiveAction {
 
     @Override
     protected void compute() {
-        if (positions.size() > THRESHOLD) {
-            ForkJoinTask.invokeAll(createSubtasks());
+        if (positions.size() <= THRESHOLD) {
+            solveBaseCase(positions);
         } else {
-            process(positions);
+            ForkJoinTask.invokeAll(divideSubtask());
         }
     }
 
-    private List<ActionManager> createSubtasks() {
+    private List<ActionManager> divideSubtask() {
         final List<ActionManager> subtask = new ArrayList<>();
         final int halfSize = positions.size() / 2;
-        final List<Position> firstHalf = positions.stream().limit(halfSize).collect(Collectors.toList());
-        final List<Position> secondHalf = positions.stream().skip(halfSize).collect(Collectors.toList());
+        final List<Position> fHalf = positions.stream().limit(halfSize).collect(Collectors.toList());
+        final List<Position> sHalf = positions.stream().skip(halfSize).collect(Collectors.toList());
 
-        subtask.add(
-                new ActionManager(firstHalf, bacteriaEnv, foodsState, maxPosition, maxFoodRadius, actionPerformer));
-        subtask.add(new ActionManager(secondHalf, bacteriaEnv, foodsState, maxPosition, maxFoodRadius,
-                actionPerformer));
+        subtask.add(new ActionManager(fHalf, bacteriaEnv, foodsState, maxPosition, maxFoodRadius, actionPerformer));
+        subtask.add(new ActionManager(sHalf, bacteriaEnv, foodsState, maxPosition, maxFoodRadius, actionPerformer));
+
         return subtask;
     }
 
-    private void process(final List<Position> positions) {
-        positions.forEach(position -> {
+    private void solveBaseCase(final List<Position> positions) {
+        positions.stream().forEach(position -> {
             final Bacteria bacteria = this.bacteriaEnv.getBacteria(position);
-            final Perception perception = createPerception(position, foodsState);
-            bacteria.setPerception(perception);
-            this.costOfLiving(bacteria);
-            this.performAction(position, bacteria);
+            bacteria.setPerception(createPerception(position));
+            costOfLiving(bacteria);
+            performAction(position, bacteria);
         });
     }
 }
